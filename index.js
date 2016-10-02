2806cc7e90f71532d8ad42bd72cef283b283bad4
Ubuntu <ubuntu@ip-172-31-33-120.us-west-2.compute.internal>
2016-10-02 07:10:02 +0000

    lets see how this goes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var fs = require("fs");
-var filename = "./public/index.html";
+var filename = 'public/index.html';
 var data = fs.readFileSync(filename);
 
+function start(resp){
+    resp.writeHead(200, {"Content-type":"text/html"});
+    resp.write(data);
+    resp.end();
+}
+
+exports.start = start;
