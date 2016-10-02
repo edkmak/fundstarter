f6b9c2fe68400f3820c395c9424559365af0f4d7
Ubuntu <ubuntu@ip-172-31-33-120.us-west-2.compute.internal>
2016-10-02 04:30:23 +0000

    make it better

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-var express = require('express')
-var app = express()
+var fs = require("fs");
+var filename = "./public/index.html";
+var data = fs.readFileSync(filename);
 
-
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
-
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
-})
